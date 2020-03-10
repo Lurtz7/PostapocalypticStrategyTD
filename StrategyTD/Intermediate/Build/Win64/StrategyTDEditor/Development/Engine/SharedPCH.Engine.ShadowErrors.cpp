e05e835597bc978ccfa7ad78aba5d96476d03a85
Lurtz7 <alexandergrahn7@gmail.com>
2020-03-10 22:45:48 +0100

    Created project

---
+++ StrategyTD/Intermediate/Build/Win64/StrategyTDEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Development/PostapocalypticStrategyTD/StrategyTD/Intermediate/Build/Win64/StrategyTDEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ StrategyTD/Intermediate/Build/Win64/UE4Editor/Inc/StrategyTD/StrategyTDGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef STRATEGYTD_StrategyTDGameMode_generated_h
+#error "StrategyTDGameMode.generated.h already included, missing '#pragma once' in StrategyTDGameMode.h"
+#endif
+#define STRATEGYTD_StrategyTDGameMode_generated_h
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_SPARSE_DATA
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_RPC_WRAPPERS
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAStrategyTDGameMode(); \
+	friend struct Z_Construct_UClass_AStrategyTDGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AStrategyTDGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/StrategyTD"), STRATEGYTD_API) \
+	DECLARE_SERIALIZER(AStrategyTDGameMode)
+
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAStrategyTDGameMode(); \
+	friend struct Z_Construct_UClass_AStrategyTDGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AStrategyTDGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/StrategyTD"), STRATEGYTD_API) \
+	DECLARE_SERIALIZER(AStrategyTDGameMode)
+
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	STRATEGYTD_API AStrategyTDGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AStrategyTDGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(STRATEGYTD_API, AStrategyTDGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AStrategyTDGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	STRATEGYTD_API AStrategyTDGameMode(AStrategyTDGameMode&&); \
+	STRATEGYTD_API AStrategyTDGameMode(const AStrategyTDGameMode&); \
+public:
+
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	STRATEGYTD_API AStrategyTDGameMode(AStrategyTDGameMode&&); \
+	STRATEGYTD_API AStrategyTDGameMode(const AStrategyTDGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(STRATEGYTD_API, AStrategyTDGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AStrategyTDGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AStrategyTDGameMode)
+
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_9_PROLOG
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_SPARSE_DATA \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_RPC_WRAPPERS \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_INCLASS \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_SPARSE_DATA \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	StrategyTD_Source_StrategyTD_StrategyTDGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> STRATEGYTD_API UClass* StaticClass<class AStrategyTDGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID StrategyTD_Source_StrategyTD_StrategyTDGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
